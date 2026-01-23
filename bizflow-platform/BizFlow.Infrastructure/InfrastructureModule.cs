04d2c5c77761b440bfbfa1aa271e434542badc66
Thienlm30 <lmthien.30@gmail.com>
2026-01-23 23:09:16 +0700

    Update .gitignore and clean up InfrastructureModule.cs

--- bizflow-platform/BizFlow.Infrastructure/InfrastructureModule.cs
+++ bizflow-platform/BizFlow.Infrastructure/InfrastructureModule.cs
@@ -4,11 +4,6 @@ using BizFlow.Infrastructure.Data;
 using BizFlow.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BizFlow.Infrastructure
 {
