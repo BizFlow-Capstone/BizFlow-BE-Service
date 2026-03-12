f3afe9c41e5ba074c86fa474ac58646573fb13f5
VuHoangHieuNgan <vuhoanghieungan564@gmail.com>
2026-03-12 12:45:43 +0700

    refactor: Simplify BusinessLocationDetailDto by inheriting from BusinessLocationDto

--- bizflow-platform/BizFlow.Application/DTOs/Location/BusinessLocationDetailDto.cs
+++ bizflow-platform/BizFlow.Application/DTOs/Location/BusinessLocationDetailDto.cs
@@ -2,17 +2,9 @@ using BizFlow.Application.DTOs.Hire;
 
 namespace BizFlow.Application.DTOs.Location
 {
-    public class BusinessLocationDetailDto
+    public class BusinessLocationDetailDto : BusinessLocationDto
     {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string Address { get; set; } = null!;
-        public string? District { get; set; }
-        public string? City { get; set; }
-        public string? Phone { get; set; }
         public string? TaxCode { get; set; }
-        public bool IsActive { get; set; }
-        public string? OwnerName { get; set; }
         public List<EmployeeSummaryDto> Employees { get; set; } = new();
     }
 }
