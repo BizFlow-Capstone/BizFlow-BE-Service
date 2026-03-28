91d9fc748fda7c18200e8fb3b489d50ce6970515
VuHoangHieuNgan <vuhoanghieungan564@gmail.com>
2026-03-29 05:26:49 +0700

    refactor: rename migration file

--- database/migrations/074_add_stripe_product_id.sql
+++ database/migrations/074_add_stripe_product_id.sql
@@ -5,5 +5,5 @@ ALTER TABLE SubscriptionPlans
     ADD COLUMN StripeProductId VARCHAR(255) NULL AFTER StripePriceId;
 
 INSERT IGNORE INTO __MigrationHistory (MigrationId, ProductVersion)
-VALUES ('050_add_stripe_product_id', '1.0.0')
+VALUES ('074_add_stripe_product_id', '1.0.0')
 ON DUPLICATE KEY UPDATE ProductVersion = '1.0.0';
--- database/migrations/077_add_deleted_at_to_subscription_plans.sql
+++ database/migrations/077_add_deleted_at_to_subscription_plans.sql
@@ -4,4 +4,4 @@ ALTER TABLE SubscriptionPlans
 CREATE INDEX idx_plan_deleted_at ON SubscriptionPlans (DeletedAt);
 
 INSERT IGNORE INTO __MigrationHistory (MigrationId, ProductVersion)
-VALUES ('053_add_deleted_at_to_subscription_plans', '1.0.0');
+VALUES ('077_add_deleted_at_to_subscription_plans', '1.0.0');
